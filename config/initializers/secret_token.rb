e064db759b5affca480600c03d527110765eeae8
Pero Mrazović <petar.mrazovic@gmail.com>
2013-08-15 11:14:44 +0200

    Created initial rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KonzumSocial::Application.config.secret_key_base = 'd2b2c5ce79690d264c72eda923882ba0204dfe80c9b9e55716136a12186239999111a11d6bbf61b22bb209d42244168618f954148ba3279d64eb74026b65d1ea'

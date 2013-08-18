e9ff02ac021bd93eaed64217a9ef91afc6a385cd
Petar Mrazovic <petar.mrazovic@gmail.com>
2013-08-18 14:51:15 +0200

    Fixing masthead-pattern problem.

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ module KonzumSocial
       Devise::UnlocksController.layout "devise"            
       Devise::PasswordsController.layout "devise"        
     end
-
-    config.assets.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

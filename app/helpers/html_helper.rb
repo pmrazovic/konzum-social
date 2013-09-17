95959b1d719f5d88083d2eb03770f97c7c3295a0
Mario <mario.volarevic@gmail.com>
2013-09-17 23:52:16 +0200

    Added helper for add to cart/ingredient/... button and changed affected views

--- app/helpers/html_helper.rb
+++ app/helpers/html_helper.rb
@@ -5,10 +5,10 @@ module HtmlHelper
       html = ''
       Category.where(:parent_id => nil).sort.each do |root_category|
         html += "<li><label class=\"tree-toggler nav-header\">#{root_category.name}</label>
-                  <ul class=\"nav nav-list tree\">
-                    #{generate_collapse_list(root_category)}
-                  </ul>
-                </li>"
+        <ul class=\"nav nav-list tree\">
+        #{generate_collapse_list(root_category)}
+        </ul>
+        </li>"
       end
       html
     end
@@ -34,10 +34,10 @@ module HtmlHelper
         html_child = "<li><a href=\"" + category_path(child) + "\">#{child.name}</a></li>"
       else
         html_child = "<li><label class=\"tree-toggler nav-header\">#{child.name}</label>
-                        <ul class=\"nav nav-list tree\">
-                          #{generate_collapse_list(child)}
-                        </ul>
-                      </li>"
+        <ul class=\"nav nav-list tree\">
+        #{generate_collapse_list(child)}
+        </ul>
+        </li>"
       end
       html += html_child
     end
@@ -59,11 +59,11 @@ module HtmlHelper
       html = ''
       Category.where(:parent_id => nil).sort.each do |root_category|
         html += "<li class=\"dropdown-submenu\">
-                        <a href=\"" + category_path(root_category) + "\">#{root_category.name}</a>
-                        <ul class=\"dropdown-menu\">
-                          #{generate_collapse_list_for_navbar(root_category)}
-                        </ul>
-                      </li>"
+        <a href=\"" + category_path(root_category) + "\">#{root_category.name}</a>
+        <ul class=\"dropdown-menu\">
+        #{generate_collapse_list_for_navbar(root_category)}
+        </ul>
+        </li>"
       end
       html
     end
@@ -89,11 +89,11 @@ module HtmlHelper
         html_child = "<li><a href=\"" + category_path(child) + "\">#{child.name}</a></li>"
       else
         html_child =  "<li class=\"dropdown-submenu\">
-                        <a href=\"" + category_path(child) + "\">#{child.name}</a>
-                        <ul class=\"dropdown-menu\">
-                          #{generate_collapse_list_for_navbar(child)}
-                        </ul>
-                      </li>"
+        <a href=\"" + category_path(child) + "\">#{child.name}</a>
+        <ul class=\"dropdown-menu\">
+        #{generate_collapse_list_for_navbar(child)}
+        </ul>
+        </li>"
       end
       html += html_child
     end  
@@ -109,4 +109,11 @@ module HtmlHelper
     end
   end
 
+  def add_btn_helper(product, size=:small, xtraArgs='', isRemote = true)                       
+    if session[:active_recipe] == nil
+      link_to "<i class =\"icon-shopping-cart\"></i> Add to cart".html_safe, cart_items_path(:product_id => product), :class => "btn btn-primary btn-#{size} #{xtraArgs}", :method => :post, :remote => true
+    else 
+      link_to "<i class=\"icon-food\"></i> Add ingredient".html_safe, ingredients_path(product_id: product), :class => "btn btn-primary btn-#{size} #{xtraArgs}", :method => :post, :remote => true         
+    end 
+  end
 end

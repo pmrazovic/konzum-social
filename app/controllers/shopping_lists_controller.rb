d0bff38efbc6eba2d732da4865bba8a5f92e6f3d
Mario <mario.volarevic@gmail.com>
2013-09-23 00:48:07 +0200

    Partially completed view

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,8 +1,15 @@
 class ShoppingListsController < ApplicationController
+  before_filter :authenticate_user!
+  before_action :set_list, only: [:show, :edit, :update, :destroy, :add_all_to_cart]
+  before_action :set_cart
+
   def show
   end
 
   def new
+    @list = current_user.shopping_lists.build
+    @list.save
+    session[:active_list] = @list.id
   end
 
   def edit
@@ -13,4 +20,14 @@ class ShoppingListsController < ApplicationController
 
   def destroy
   end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_recipe
+    @list = ShoppingList.find(params[:id])
+  end
+
+  def set_cart
+    @cart = current_cart
+  end  
 end
